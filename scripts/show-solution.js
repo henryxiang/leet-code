9c8b030fff7b47aca1ab5543bfba19db1df54131
Henry Xiang <hxxiang@ucdavis.edu>
2019-12-08 11:06:40 -0800

    fixed HTML entity decoding error

--- scripts/show-solution.js
+++ scripts/show-solution.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 
+const Entities = require('html-entities').AllHtmlEntities;
 const { connect, search } = require('./database');
 
 const id = process.argv[2];
@@ -9,20 +10,7 @@ const query = {
 };
 
 function decodeEntities(encodedString) {
-  const translate_re = /&(nbsp|amp|quot|lt|gt);/g;
-  const translate = {
-      'nbsp': ' ',
-      'amp' : '&',
-      'quot': '"',
-      'lt'  : '<',
-      'gt'  : '>'
-  };
-  return encodedString.replace(translate_re, function(match, entity) {
-      return translate[entity];
-  }).replace(/&#(\d+);/gi, function(match, numStr) {
-      var num = parseInt(numStr, 10);
-      return String.fromCharCode(num);
-  });
+  return (new Entities).decode(encodedString);
 }
 
 (async function() {
